be6458a9136677c36bb7cbda77413712480ec04c
Aleksi Vartiainen <aleksi.vartiainen@live.fi>
2017-11-19 12:36:56 +0200

    Camera action + visual API

--- PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/MainActivity.java
+++ PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/MainActivity.java
@@ -4,14 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.ListAdapter;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
