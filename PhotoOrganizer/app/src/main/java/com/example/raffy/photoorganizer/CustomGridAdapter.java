7d892f9a8e89722ab4994040699467e1ca413f43
Aleksi Vartiainen <aleksi.vartiainen@live.fi>
2017-11-19 14:30:02 +0200

    Main menu adapter: fix bux where multiline text gets cut off

--- PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/CustomGridAdapter.java
+++ PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/CustomGridAdapter.java
@@ -8,97 +8,66 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-/**
- * Created by Raffy on 10/11/2017.
- */
-
 public class CustomGridAdapter extends BaseAdapter {
 
     private Context context;
     private final String[] gridValues;
 
     //Constructor to initialize values
-    public CustomGridAdapter(Context context, String[ ] gridValues) {
-
+    CustomGridAdapter(Context context, String[] gridValues) {
         this.context        = context;
         this.gridValues     = gridValues;
     }
 
     @Override
     public int getCount() {
-
         // Number of times getView method call depends upon gridValues.length
         return gridValues.length;
     }
 
     @Override
     public Object getItem(int position) {
-
-        return null;
+        return null;    // TODO possibly to be implemented
     }
 
     @Override
     public long getItemId(int position) {
-
-        return 0;
+        return 0;       // TODO possibly to be implemented
     }
 
-
     // Number of times getView method call depends upon gridValues.length
-
     public View getView(int position, View convertView, ViewGroup parent) {
-
-        // LayoutInflator to call external grid_item.xml file
-
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
         View gridView;
-
         if (convertView == null) {
-
-            gridView = new View(context);
-
             // get layout from grid_item.xml ( Defined Below )
-
-            gridView = inflater.inflate( R.layout.grid_item , null);
+            gridView = LayoutInflater.from(context).inflate( R.layout.grid_item , parent, false);
 
             // set value into textview
-
-            TextView textView = (TextView) gridView
-                    .findViewById(R.id.text);
-
+            TextView textView = gridView.findViewById(R.id.text);
             textView.setText(gridValues[position]);
+            textView.setLines(2);
 
             // set image based on selected text
+            ImageView imageView = gridView.findViewById(R.id.imageButton);
 
-            ImageView imageView = (ImageView) gridView
-                    .findViewById(R.id.imageButton);
-
-            String arrLabel = gridValues[ position ];
-
-            if (arrLabel.equals("Take photo")) {
-
-                imageView.setImageResource(R.drawable.camera);
-
-            } else if (arrLabel.equals("Gallery")) {
-
-                imageView.setImageResource(R.drawable.gallery);
-
-            } else if (arrLabel.equals("Group management")) {
-
-                imageView.setImageResource(R.drawable.partner);
-
-            } else {
-
-                imageView.setImageResource(R.drawable.settings);
+            String arrLabel = gridValues[position];
+            switch (arrLabel) {
+                case "Take photo":
+                    imageView.setImageResource(R.drawable.camera);
+                    break;
+                case "Gallery":
+                    imageView.setImageResource(R.drawable.gallery);
+                    break;
+                case "Group management":
+                    imageView.setImageResource(R.drawable.partner);
+                    break;
+                case "Settings":
+                    imageView.setImageResource(R.drawable.settings);
+                    break;
             }
-
         } else {
-
-            gridView = (View) convertView;
+            gridView = convertView;
         }
-
         return gridView;
     }
 }
