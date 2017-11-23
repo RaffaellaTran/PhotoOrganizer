9bed5dccdae2d916a25632c01e77202d3846900d
Sami Lappalainen <sami.k.lappalainen@iki.fi>
2017-11-23 16:35:57 +0200

    Added handlers for creating, joining and leaving groups

--- OrganizerBackend/main.py
+++ OrganizerBackend/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
-from flask.ext.api import status
+from flask import Flask, request, jsonify, Response
+#from flask.ext.api import status
 import google.auth.transport.requests
 import google.oauth2.id_token
 from firebase import firebase
@@ -9,29 +9,57 @@ from settings import *
 from firebase_admin import auth
 
 app = Flask(__name__)
-global count = 1
 
 
-#
 @app.route('/create_group', methods=['POST'])
 def create_group():
 
+    data = request.values
+
+    #Uncomment these to check for uid_token
+    #id_token = data['token']
+    #decoded_token = auth.verify_token(id_token)
+    #uid = decoded_token['uid']
+
     fb = firebase.FirebaseApplication(FIREBASE_PROJECT_URL, None)
-    group_name = request.group_name
-    expiration_time = request.expiration_time
-    owner = request.owner
-    putdata = {'group_name' : group_name, 'owner': owner, 'expiration_time': expiration_time }
-    fb.put('/groups', group_name, putdata)
-    return status.HTTP_200_OK
+
+    group_name = data['group_name']
+    expiration_time = data['expiration_time']
+    owner = data['owner']
+
+    putdata = {'owner': owner, 'expiration_time': expiration_time }
+    response = fb.put('/groups',group_name, putdata)
+    return jsonify(response)
 
 
 
 
 #Join
-@app.route('/join_group', methods=['GET', 'POST'])
+@app.route('/join_group', methods=['POST'])
 def join_group():
-    return status.HTTP_204_NO_CONTENT
+    data = request.values
 
+    fb = firebase.FirebaseApplication(FIREBASE_PROJECT_URL, None)
+
+    join_token = data['join_token']
+    group_name = data['group_name']
+    user = data['user']
+
+    putdata = {user : 'true'}
+    response = fb.put('/groups/' + group_name, user, True)
+    return jsonify(response)
+
+
+@app.route('/leave_group', methods=['DELETE'])
+def leave_group():
+    data = request.values
+    fb = firebase.FirebaseApplication(FIREBASE_PROJECT_URL, None)
+
+    user = data['user']
+    group_name = data['group_name']
+
+    response = fb.delete('/groups/' + group_name, user)
+    return jsonify(response)
 
 
 @app.route('/label', methods=['GET','POST'])
