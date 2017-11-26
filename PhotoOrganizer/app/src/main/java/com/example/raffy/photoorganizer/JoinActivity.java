138e8e636fefc52de2bf88d7388988fe422f48bc
Aleksi Vartiainen <aleksi.vartiainen@live.fi>
2017-11-26 12:43:46 +0200

    Add Async task for JoinActivity

--- PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/JoinActivity.java
+++ PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/JoinActivity.java
@@ -1,7 +1,10 @@
 package com.example.raffy.photoorganizer;
 
+import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,8 +12,19 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.SparseArray;
 import android.widget.FrameLayout;
 import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+import java.lang.ref.WeakReference;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static com.example.raffy.photoorganizer.QRCameraPreview.getCameraInstance;
 
@@ -23,6 +37,7 @@ public class JoinActivity extends AppCompatActivity {
     public static final int PERMISSIONS_REQUEST_CAMERA = 1;
     private Camera mCamera;
     private QRCameraPreview mPreview;
+    private Timer mTimer;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,7 +79,55 @@ public class JoinActivity extends AppCompatActivity {
         mCamera = getCameraInstance();
         // Create our Preview view and set it as the content of our activity.
         mPreview = new QRCameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        FrameLayout preview = findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+
+        // Timer
+        mTimer = new Timer();
+        mTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                //Bitmap bitmap = mPreview.getBitmap();
+                //new ExamineImageTask(JoinActivity.this).execute(bitmap);
+            }
+        }, 0, 1000);
+    }
+
+    /**
+     * Use a static class to prevent leaks.
+     */
+
+    private static class ExamineImageTask extends AsyncTask<Bitmap, Void, Void> {
+
+        private WeakReference<Activity> context;
+
+        ExamineImageTask(Activity context) {
+            this.context = new WeakReference<>(context);
+        }
+
+        @Override
+        protected Void doInBackground(Bitmap... bitmaps) {
+            BarcodeDetector codeDetector = new BarcodeDetector.Builder(context.get())
+                    .setBarcodeFormats(Barcode.QR_CODE)
+                    .build();
+            for (Bitmap bitmap : bitmaps) {
+                float ratio = 1200.0f / Math.max(bitmap.getWidth(), bitmap.getHeight());
+                final Bitmap scaled = Bitmap.createScaledBitmap(bitmap, Math.round(bitmap.getWidth() * ratio), Math.round(bitmap.getHeight() * ratio), false);
+                Frame frame = new Frame.Builder().setBitmap(scaled).build();
+                final SparseArray<Barcode> barcodes = codeDetector.detect(frame);
+                context.get().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // TODO
+                        if (barcodes.size() > 0) {
+                            Toast.makeText(context.get(), "Barcode found", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(context.get(), "No barcode", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+            return null;
+        }
     }
 }
