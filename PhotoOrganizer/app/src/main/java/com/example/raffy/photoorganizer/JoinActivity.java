64e3c205f8acc64503485b9aa8ca4da487ba744e
Aleksi Vartiainen <aleksi.vartiainen@live.fi>
2017-11-26 12:13:52 +0200

    Camera intent for Join group

--- PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/JoinActivity.java
+++ PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/JoinActivity.java
@@ -1,7 +1,18 @@
 package com.example.raffy.photoorganizer;
 
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import static com.example.raffy.photoorganizer.QRCameraPreview.getCameraInstance;
 
 /**
  * Use this dummy activity to wrap methods regarding the join group camera intent.
@@ -9,9 +20,45 @@ import android.support.v7.app.AppCompatActivity;
 
 public class JoinActivity extends AppCompatActivity {
 
+    public static final int PERMISSIONS_REQUEST_CAMERA = 1;
+    private Camera mCamera;
+    private QRCameraPreview mPreview;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_group_layout);
+
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{android.Manifest.permission.CAMERA},
+                        PERMISSIONS_REQUEST_CAMERA);
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.CAMERA},
+                        PERMISSIONS_REQUEST_CAMERA);
+            }
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAMERA:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Create an instance of Camera
+                    mCamera = getCameraInstance();
+                    // Create our Preview view and set it as the content of our activity.
+                    mPreview = new QRCameraPreview(this, mCamera);
+                    FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+                    preview.addView(mPreview);
+                } else {
+                    Toast.makeText(this, "Camera permission is denied!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 }
