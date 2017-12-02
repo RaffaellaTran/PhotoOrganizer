84983773639ba635e87986ba6f7140e9c94b7ad1
Aleksi Vartiainen <aleksi.vartiainen@live.fi>
2017-12-02 12:50:41 +0200

    Delete group

--- PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/GroupManagementActivity.java
+++ PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/GroupManagementActivity.java
@@ -1,16 +1,38 @@
 package com.example.raffy.photoorganizer;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.Calendar;
+
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class GroupManagementActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -91,7 +113,7 @@ public class GroupManagementActivity extends AppCompatActivity implements View.O
                         .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
-                                // TODO
+                                startDeleteGroupAction(GroupManagementActivity.this, mGroup);
                             }
                         }).setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
                             @Override
@@ -110,6 +132,72 @@ public class GroupManagementActivity extends AppCompatActivity implements View.O
         }
     }
 
+    private static void startDeleteGroupAction(final Activity context, final Group group) {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) return;
+        final ProgressDialog progress = new ProgressDialog(context);
+        progress.show();
+        // get token and start progress
+        user.getIdToken(true).addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+            @Override
+            public void onComplete(@NonNull Task<GetTokenResult> task) {
+                String token = task.getResult().getToken();
+                RequestBody body = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("token", token)
+                        .addFormDataPart("group_name", group.getName())
+                        .build();
+                Request request = new Request.Builder()
+                        .url("http://10.0.2.2:5000/leave_group")  // TODO
+                        .delete(body)
+                        .build();
+                new Http(context, token, progress).execute(request);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("!!!", e.getMessage());
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
+    private static class Http extends AsyncTask<Request, Void, String> {
+
+        private WeakReference<Activity> context;
+        private String token;
+        private ProgressDialog progress;
+
+        Http(Activity context, String token, ProgressDialog progress) {
+            this.context = new WeakReference<>(context);
+            this.token = token;
+            this.progress = progress;
+        }
+
+        @Override
+        protected String doInBackground(Request... requests) {
+            OkHttpClient client = new OkHttpClient();
+            for (Request request : requests) {
+                try {
+                    Response response = client.newCall(request).execute();
+                    context.get().finish();
+                    return "Success! " + response.toString();
+                } catch (IOException e) {
+                    return "Network error: " + e.getMessage();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String message) {
+            super.onPostExecute(message);
+            if (message != null) {
+                Log.i("GROUPS", message);
+                Toast.makeText(context.get(), message, Toast.LENGTH_LONG).show();
+            }
+            progress.dismiss();
+        }
+    }
 
 }
