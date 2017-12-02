423b1b7f08fb610901ed33ff57b5545c68d98df7
Oscar Stigzelius <oscar.stigzelius@aalto.fi>
2017-12-02 17:44:05 +0200

    Made front end screen work

--- PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/SettingsActivity.java
+++ PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/SettingsActivity.java
@@ -1,8 +1,13 @@
 package com.example.raffy.photoorganizer;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.ToggleButton;
 
 /**
  * Created by Raffy on 15/11/2017.
@@ -10,9 +15,19 @@ import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    /*
     // Switches
     ImageQualitySwitches wifiSwitches = new ImageQualitySwitches();
     ImageQualitySwitches mobileSwitches = new ImageQualitySwitches();
+
+    // ToggleButtons
+    ImageQualityToggleBtn wifiToggleBtn = new ImageQualityToggleBtn();
+    ImageQualityToggleBtn mobileToggleBtn = new ImageQualityToggleBtn();
+    */
+
+    // RadioButtons
+    ImageQualityRadioBtn wifiRadioBtn = new ImageQualityRadioBtn();
+    ImageQualityRadioBtn mobileRadioBtn = new ImageQualityRadioBtn();
 
     // Image Quality
     ImageQuality wifiQuality = new ImageQuality();
@@ -31,6 +46,20 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        this.wifiRadioBtn.low = (RadioButton) findViewById(R.id.wifiRadioBtnLow);
+        this.wifiRadioBtn.high = (RadioButton) findViewById(R.id.wifiRadioBtnHigh);
+        this.wifiRadioBtn.full = (RadioButton) findViewById(R.id.wifiRadioBtnFull);
+        this.wifiRadioBtn.radioBtns = (RadioGroup) findViewById(R.id.wifiRadioGroup);
+
+        this.mobileRadioBtn.low = (RadioButton) findViewById(R.id.mobileRadioBtnLow);
+        this.mobileRadioBtn.high = (RadioButton) findViewById(R.id.mobileRadioBtnHigh);
+        this.mobileRadioBtn.full = (RadioButton) findViewById(R.id.mobileRadioBtnFull);
+        this.mobileRadioBtn.radioBtns = (RadioGroup) findViewById(R.id.mobileRadioGroup);
+
+        setWifiImageQuality();
+        setMobileImageQuality();
+
+        /*
         this.wifiSwitches.low = (Switch) findViewById(R.id.lowWifiSwitch);
         this.mobileSwitches.low = (Switch) findViewById(R.id.lowMobileSwitch);
         this.wifiSwitches.high = (Switch) findViewById(R.id.highWifiSwitch);
@@ -48,45 +77,38 @@ public class SettingsActivity extends AppCompatActivity {
 
         setImageQuality(this.wifiSwitches, this.wifiQuality);
         setImageQuality(this.mobileSwitches, this.mobileQuality);
+        */
     }
 
-    public void setImageQuality(ImageQualitySwitches iqs, ImageQuality iq) {
-        if (iqs.low.isChecked()) {
-            iq.setWidth(640);
-            iq.setHeight(480);
-            iq.setQuality(Quality.LOW.toString());
+    public void setWifiImageQuality() {
+        if (this.wifiRadioBtn.getActiveRadioBtn().equals(this.wifiRadioBtn.low)) {
+            this.wifiQuality.setImageQualityToLow();
         }
-        else if (iqs.high.isChecked()) {
-            iq.setWidth(1280);
-            iq.setHeight(960);
-            iq.setQuality(Quality.HIGH.toString());
+        else if (this.wifiRadioBtn.getActiveRadioBtn().equals(this.wifiRadioBtn.high)) {
+            this.wifiQuality.setImageQualityToHigh();
         }
-        else if (iqs.full.isChecked()) {
-            iq.setQuality(Quality.FULL.toString());
+        else if (this.wifiRadioBtn.getActiveRadioBtn().equals(this.wifiRadioBtn.full)) {
+            this.wifiQuality.setImageQualityToFull();
         }
-
         else {
-            iq.setWidth(1280);
-            iq.setHeight(960);
-            iq.setQuality(Quality.HIGH.toString());
+            this.wifiQuality.setImageQualityToHigh();
         }
     }
 
-    /*
-    public boolean OneSwitchIsOn() {
-        if (this.wifiSwitches.atleastOneSwitchIsTrue() || this.mobileSwitches.atleastOneSwitchIsTrue()) {
-            return true;
+    public void setMobileImageQuality() {
+        if (this.mobileRadioBtn.getActiveRadioBtn().equals(this.mobileRadioBtn.low)) {
+            this.mobileQuality.setImageQualityToLow();
         }
-        return false;
+        else if (this.mobileRadioBtn.getActiveRadioBtn().equals(this.mobileRadioBtn.high)) {
+            this.mobileQuality.setImageQualityToHigh();
+        }
+        else if (this.mobileRadioBtn.getActiveRadioBtn().equals(this.mobileRadioBtn.full)) {
+            this.mobileQuality.setImageQualityToFull();
+        }
+        else {
+            this.mobileQuality.setImageQualityToHigh();
+        }
     }
-    */
-
-
-
-
-    // ADD function for only one switch can be "ON" @ a time
-    // ADD default value
-    // SAVE the value that was last turned "ON"
 }
 
 class ImageQualitySwitches {
@@ -123,10 +145,78 @@ class ImageQualitySwitches {
     }
 }
 
+class ImageQualityToggleBtn {
+
+    ToggleButton low;
+    ToggleButton high;
+    ToggleButton full;
+
+    boolean toggleButtonIsOn(ToggleButton t) {
+        return t.isChecked();
+    }
+
+    void setToggleButtonToTrueOthersToFalse(Switch s) {
+        s.setChecked(true);
+
+        this.full.isClickable();
+
+        if (!s.equals(this.low)) {
+            this.low.setChecked(false);
+        }
+
+        if (!s.equals(this.high)) {
+            this.high.setChecked(false);
+        }
+
+        if (!s.equals(this.full)) {
+            this.full.setChecked(false);
+        }
+    }
+
+    boolean atleastOneSwitchIsTrue() {
+        if (this.low.isChecked() || this.high.isChecked() || this.full.isChecked()) {
+            return true;
+        }
+        return false;
+    }
+}
+
+class ImageQualityRadioBtn {
+
+    RadioButton low;
+    RadioButton high;
+    RadioButton full;
+    RadioGroup radioBtns;
+
+    public RadioButton getActiveRadioBtn() {
+        int id = this.radioBtns.getCheckedRadioButtonId();
+        return getRadioBtnWithIndex(id);
+    }
+
+    public RadioButton getRadioBtnWithIndex(int id) {
+        if (id == this.low.getId()) {
+            return this.low;
+        }
+        else if (id == this.high.getId()) {
+            return this.high;
+        }
+        else if (id == this.full.getId()) {
+            return this.full;
+        }
+        else {
+            Log.d("INDEXERROR", "getRadioBtnWithIndex: No such radiobutton with index: " + id);
+            throw new IndexOutOfBoundsException("No such radiobutton with index: " + id);
+        }
+    }
+}
+
 class ImageQuality {
     int width;
     int height;
     String quality;
+    String LOW = "low";
+    String HIGH = "high";
+    String FULL = "full";
 
     public void setWidth(int width) {
         this.width = width;
@@ -150,6 +240,22 @@ class ImageQuality {
 
     public String getQuality() {
         return quality;
+    }
+
+    public void setImageQualityToLow() {
+        this.setWidth(640);
+        this.setHeight(480);
+        this.setQuality(LOW);
+    }
+
+    public void setImageQualityToHigh() {
+        this.setWidth(1280);
+        this.setHeight(960);
+        this.setQuality(HIGH);
+    }
+
+    public void setImageQualityToFull() {
+        this.setQuality(FULL);
     }
 }
 
