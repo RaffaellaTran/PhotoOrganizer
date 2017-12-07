b6a15af32b6c1a4fae953dfd611a88b5515d13d7
Oscar Stigzelius <oscar.stigzelius@aalto.fi>
2017-12-07 23:48:05 +0200

    Updated Settings and SettingsHelper

--- PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/SettingsHelper.java
+++ PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/SettingsHelper.java
@@ -2,56 +2,88 @@ package com.example.raffy.photoorganizer;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 import com.google.firebase.storage.FirebaseStorage;
 
+import static com.example.raffy.photoorganizer.SettingsHelper.ImageQuality.*;
+
 /**
- * Created by Anton on 4.12.2017.
+ * Created by oscarstigzelius on 05/12/2017.
  */
 
 public class SettingsHelper {
 
+    // Strings
+    public static final String WIFI_IMAGE_QUALITY = "WifiImageQuality";
+    public static final String MOBILE_IMAGE_QUALITY = "MobileImageQuality";
     public static final String BACKEND_URL = "https://mcc-fall-2017-g08.appspot.com";
-
     public static final String BUCKET_SMALL = "gs://mcc-fall-2017-g08_small";
     public static final String BUCKET_LARGE = "gs://mcc-fall-2017-g08_large";
 
-    private static final SettingsHelper ourInstance = new SettingsHelper();
-
-    public static SettingsHelper getInstance() {
-        return ourInstance;
-    }
-
+    // SharedPreferences
     public static final String PREFERENCES = "preferences";
-    SharedPreferences sharedPreferences = null;
-
-    private SettingsHelper() {
-    }
+    SharedPreferences sharedpreferences;
+    Context context;
 
     public enum ImageQuality {
-        SMALL, LARGE, ORIGINAL
+        LOW, HIGH, FULL
     }
 
-    public static ImageQuality getImageQuality(Context context) {
-        SettingsHelper instance = getInstance();
-        if (instance.sharedPreferences == null) {
-            instance.sharedPreferences = context.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+    public SettingsHelper(Context context) {
+        this.context = context;
+        this.sharedpreferences = this.context.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+    }
+
+    public String getString(String key, String defaultValue) {
+        return this.sharedpreferences.getString(key, defaultValue);
+    }
+
+    public String getWifiImageQuality() {
+        return this.getString(WIFI_IMAGE_QUALITY, HIGH.toString());
+    }
+
+    public String getMobileImageQuality() {
+        return this.getString(MOBILE_IMAGE_QUALITY, HIGH.toString());
+    }
+
+    // Returns the right ImageQuality, depending if you are connected to Wifi or not
+    public String getImageQuality() {
+        ConnectivityManager connManager = (ConnectivityManager) this.context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        if (mWifi != null && mWifi.isConnected()) {
+            return getWifiImageQuality();
         }
-        // TODO
-        return ImageQuality.ORIGINAL;
+
+        return getMobileImageQuality();
     }
 
-    public static FirebaseStorage getFirebaseStorage(ImageQuality quality) {
+    public void editString(String key, String value) {
+        SharedPreferences.Editor editor = this.sharedpreferences.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public void editWifiImageQuality(String value) {
+        this.editString(WIFI_IMAGE_QUALITY, value);
+    }
+
+    public void editMobileImageQuality(String value) {
+        this.editString(MOBILE_IMAGE_QUALITY, value);
+    }
+
+    public FirebaseStorage getFirebaseStorage(String quality) {
         switch (quality) {
-            case ORIGINAL:
+            case "FULL":
                 return FirebaseStorage.getInstance();
-            case SMALL:
+            case "LOW":
                 return FirebaseStorage.getInstance(BUCKET_SMALL);
-            case LARGE:
+            case "HIGH":
                 return FirebaseStorage.getInstance(BUCKET_LARGE);
             default:
                 return FirebaseStorage.getInstance();
         }
     }
-
 }
