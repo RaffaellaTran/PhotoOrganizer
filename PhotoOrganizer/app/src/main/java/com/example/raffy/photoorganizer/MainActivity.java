39563305f9ec019224157fa2a6fa44643597bcb7
Anton Debner <anton.debner@aalto.fi>
2017-12-09 16:51:15 +0200

    Implement push notifications

--- PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/MainActivity.java
+++ PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.raffy.photoorganizer;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -32,6 +35,17 @@ public class MainActivity extends AppCompatActivity {
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
+
+        // Set push-notification related settings
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            String channelId  = getString(R.string.default_notification_channel_id);
+            String channelName = getString(R.string.default_notification_channel_name);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
         
         gridView = (GridView) findViewById(R.id.gridView);
         CustomGridAdapter customGridAdapter = new CustomGridAdapter(getApplicationContext(), data);
