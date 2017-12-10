8c616550b0d21dced5b600f9bbdffa1663528bbe
Anton Debner <anton.debner@aalto.fi>
2017-12-10 23:05:12 +0200

    Get group name

--- web app/gallery.js
+++ web app/gallery.js
@@ -1,4 +1,4 @@
-(function(){
+//(function(){
 	// Initialize Firebase
  var config = {
     apiKey: "AIzaSyA-RE77NPqD1m0zsw5jprChVAckh8nq-4M",
@@ -9,91 +9,78 @@
     messagingSenderId: "672954146858"
   };
   firebase.initializeApp(config);
-  
+
   var imgName = 'hi.jpeg';
   var img= '<div class="row" id="img"><div class="col-md-2"><img class="img"  src="'+ imgName +'" ></div></div>';
-  
+
   //Add a realtime listener
   firebase.auth().onAuthStateChanged(firebaseUser=>{
 	 if (firebaseUser){
 		 console.log(firebaseUser);
+     getGroupName(firebaseUser);
 	 } else{
 		 console.log('not logged in');
 	 }
-	  
+ })
+
   //});
 	const btnOut= document.getElementById('btnSignOut');
 	btnOut.addEventListener('click', e=> {
-		
+
 		firebase.auth().signOut();
 	});
-	
-	//Get elements
-	const preObject=document.getElementById('object');
-	const ulList= document.getElementById('list');
-	
-	//Create reference
-	const dbRefObject= firebase.database().ref().child('pictures');
-	const dbRefList= dbRefObject.child('TestGroup2');
-	const dbimg= dbRefList.child('-L-XgfdJxKPfdTBGJOw8');
-	const imagess= dbRefList.child('bucket_identifier');
-	
-	//download
-	//var fileName = window.AppInventor.getWebViewString();
-	var storage    = firebase.storage();
-	var storageRef = storage.ref();
-	//var pathReference = storage.ref(fileName);
-//	document.getElementById("mess").innerHTML =pathReference;
-            /*      pathReference.getDownloadURL().then(function(url) {
-                  window.AppInventor.setWebViewString(url);
- });*/
-	for (var i = 0; i < dbRefList.length; i++){
-    var obj = dbRefList[i];
-    for (var key in obj){
-        var attrName = key;
-        var attrValue = obj[key];
-		
-    }
-}
-	//Sync object change
-	dbimg.on('value', snap=>{
-		
-		preObject.innerText= JSON.stringify(snap.val(), null,3);
-		img.innerHTML= attrValue;
-	//	document.getElementById("mess").innerHTML = JSON.stringify(snap.val(), null,3);
-	//	document.getElementById("mess").innerHTML =pathReference;
-	//	if (key='bucket_identifier'){document.getElementById("mess").innerHTML =snap.val();}
-	});
-	
-	
-}());
+
+  function getGroupName(firebaseUser) {
+    //Get elements
+  	const preObject=document.getElementById('object');
+  	const ulList= document.getElementById('list');
+
+    // Get group name
+    var userId = firebase.auth().currentUser.uid;
+    return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+      groupName = snapshot.val()
+      console.log(groupName)
+      startListeningToImages(firebaseUser, groupName)
+    });
 
 
 
+  function startListeningToImages(firebaseUser, groupName) {
+    //Create reference
+  	const dbRefObject= firebase.database().ref().child('pictures');
+  	const dbRefList= dbRefObject.child('TestGroup2');
+  	const dbimg= dbRefList.child('-L-XgfdJxKPfdTBGJOw8');
+  	const imagess= dbRefList.child('bucket_identifier');
+
+  	//download
+  	//var fileName = window.AppInventor.getWebViewString();
+  	var storage    = firebase.storage();
+  	var storageRef = storage.ref();
+  	//var pathReference = storage.ref(fileName);
+  //	document.getElementById("mess").innerHTML =pathReference;
+              /*      pathReference.getDownloadURL().then(function(url) {
+                    window.AppInventor.setWebViewString(url);
+   });*/
+  	for (var i = 0; i < dbRefList.length; i++){
+      var obj = dbRefList[i];
+      for (var key in obj){
+          var attrName = key;
+          var attrValue = obj[key];
+
+      }
+  }
+  	//Sync object change
+  	dbimg.on('value', snap=>{
+
+  		preObject.innerText= JSON.stringify(snap.val(), null,3);
+  		img.innerHTML= attrValue;
+  	//	document.getElementById("mess").innerHTML = JSON.stringify(snap.val(), null,3);
+  	//	document.getElementById("mess").innerHTML =pathReference;
+  	//	if (key='bucket_identifier'){document.getElementById("mess").innerHTML =snap.val();}
+  	});
+  }
+  }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//}());
