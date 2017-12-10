5047c6ec17f2e87060b6639ddfab8dec7523f420
Sami Lappalainen <sami.k.lappalainen@iki.fi>
2017-12-10 20:32:02 +0200

    Working deploy script

--- OrganizerBackend/deploy.sh
+++ OrganizerBackend/deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-docker build -t test:latest .
-docker tag test us.gcr.io/mcc-fall-2017-g08/test
-gcloud docker -- push us.gcr.io/mcc-fall-2017-g08/test
-gcloud app deploy --image-url us.gcr.io/mcc-fall-2017-g08/test
-gcloud app deploy cron.yaml
-gcloud app logs tail -s default
+set -x
+if [ $1 == 'docker' ]; then
+    mv Dockerfile_temp Dockerfile
+    docker build -t test:latest .
+    docker tag test us.gcr.io/mcc-fall-2017-g08/test
+    gcloud docker -- push us.gcr.io/mcc-fall-2017-g08/test
+    gcloud app deploy --image-url us.gcr.io/mcc-fall-2017-g08/test
+    gcloud app deploy cron.yaml
+    gcloud app logs tail -s default
+else
+    mv Dockerfile Dockerfile_temp
+    gcloud app deploy
+    gcloud app deploy cron.yaml
+    gcloud app logs tail -s default
+fi
