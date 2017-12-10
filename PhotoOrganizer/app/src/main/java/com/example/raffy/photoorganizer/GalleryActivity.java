6183d09766caad99cf6295513c130f75e89d6b42
Raffaella <raffaella.tran@gmail.com>
2017-12-10 11:18:37 +0200

    fix error

--- PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/GalleryActivity.java
+++ PhotoOrganizer/app/src/main/java/com/example/raffy/photoorganizer/GalleryActivity.java
@@ -26,7 +26,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
